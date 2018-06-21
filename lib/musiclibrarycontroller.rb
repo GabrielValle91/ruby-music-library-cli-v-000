84114435a91627eb7220b08c34ec8d151f5a9929
Gabriel Valle <gvalle91@gmail.com>
2018-06-21 18:40:26 +0000

    automatically backed up by learn

--- lib/musiclibrarycontroller.rb
+++ lib/musiclibrarycontroller.rb
@@ -12,7 +12,7 @@ class MusicLibraryController
     puts "Welcome to your music library!"
     answer = ""
     options = ["list songs" , "list genres" , "list artist" , "list genre" , "play song" , "exit"]
-    self.update_song_list
+    
     while !options.include?(answer) do
       puts "To list all of your songs, enter 'list songs'."
       puts "To list all of the artists in your library, enter 'list artists'."
@@ -46,7 +46,7 @@ class MusicLibraryController
 
   def list_songs
     #song_list = []
-
+    self.update_song_list
     #sort songs alphabetically
     @song_list.sort_by! {|obj| obj.name}
     #puts each song with a number in front of it
