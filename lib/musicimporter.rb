65951f1b15d551dc46f1201430c55121f42823c3
Gabriel Valle <gvalle91@gmail.com>
2018-06-21 17:15:15 +0000

    automatically backed up by learn

--- lib/musicimporter.rb
+++ lib/musicimporter.rb
@@ -13,13 +13,14 @@ class MusicImporter
     Dir["#{@path}/**/*.mp3"].each do |file_name|
       @files << file_name.gsub!(/.\/spec\/fixtures\/mp3s\//,"")
     end
-    binding.pry
+    #binding.pry
 
     @files
   end
 
   def import
     #@files = @files.uniq
+    self.files
     binding.pry
     @files.each do |file_name|
       Song.create_from_filename(file_name)
