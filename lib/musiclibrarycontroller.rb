54e55ebeedc31f9209371f255297d03539f2883b
Gabriel Valle <gvalle91@gmail.com>
2018-06-21 19:29:15 +0000

    automatically backed up by learn

--- lib/musiclibrarycontroller.rb
+++ lib/musiclibrarycontroller.rb
@@ -41,7 +41,9 @@ class MusicLibraryController
     answer_adj = answer - 1
     song_list = self.list_songs
     if (answer_adj) < song_list.length
-      chosen_song = song_list[answer_adj]
+      chosen_song = song_list[answer_adj].split(" - ")
+      #{counter}. #{song.artist.name} - #{song.name} - #{song.genre.name}"
+      puts "Playing #{chosen_song[1]} by #{chosen_song[0].split(".")[1]}"
     end
 
   end
