0c491c001ee56482448feb1c13242c8bd85da699
Gabriel Valle <gvalle91@gmail.com>
2018-06-21 17:20:48 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -31,11 +31,13 @@ class Song
 
   def self.new_from_filename(file_name)
     #binding.pry
-    file_array = file_name.to_s.split(" - ")
-    new_song = self.find_or_create_by_name(file_array[1])
-    new_song.artist = file_array[0]
-    new_song.genre = file_array[2].slice(0,file_array[2].length - 4)
-    new_song
+    if file_name.class == String
+      file_array = file_name.split(" - ")
+      new_song = self.find_or_create_by_name(file_array[1])
+      new_song.artist = file_array[0]
+      new_song.genre = file_array[2].slice(0,file_array[2].length - 4)
+      new_song
+    end
   end
 
   def self.create_from_filename(file_name)
